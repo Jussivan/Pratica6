e05deb51432cba37df8ac70e74387afb0873e265
Jussivan <115298615+Jussivan@users.noreply.github.com>
2022-10-14 16:54:23 -0300

    Add files via upload

---
+++ Desconto.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main () {
+	setlocale(LC_ALL, "Portuguese_Brazil");
+	int pessoaA, pessoaB;
+	printf("Informe a idade da Pessoa A:");
+	scanf("%d", &pessoaA);
+	printf("Informe a idade da Pessoa B:");
+	scanf("%d", &pessoaB);
+	if(pessoaA%2==0) {
+		printf("DESCONTO!");
+	}
+	return 0;
+}
